bb88984ecfc8070b6b27dd5c7829de3e192a816f
ArifImran-apex <arif.imran@breakthru.pk>
2023-01-27 23:53:45 +0500

    Add project files.

---
+++ XamarinTestApp/BaseContentPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinTestApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BaseContentPage : ContentPage
+    {
+        private bool absoluteLayoutWrapped;
+        public BaseContentPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnChildAdded(Element child)
+        {
+            base.OnChildAdded(child);
+            if (!absoluteLayoutWrapped)
+            {
+                absoluteLayoutWrapped = true;
+                var originalView = this.Content;
+                var parentAbsLayout = new AbsoluteLayout();
+                parentAbsLayout.Children.Add(originalView);
+                var balControl = new BalloonControl();
+                AbsoluteLayout.SetLayoutFlags(balControl, AbsoluteLayoutFlags.All);
+                AbsoluteLayout.SetLayoutBounds(balControl, new Rectangle(0.5,0.9,0.5,0.1));
+                parentAbsLayout.Children.Add(balControl);
+                this.Content = parentAbsLayout;
+            }
+        }
+    }
+}
---
+++ XamarinTestApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.CommunityToolkit.UI.Views.Options;
+using Xamarin.Forms;
+
+namespace XamarinTestApp
+{
+    public partial class MainPage : BaseContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var messageOptions = new MessageOptions
+            {
+                Message = "Toast with Padding and round corner",
+                Foreground = Color.White,
+                Font = Font.SystemFontOfSize(15,FontAttributes.Italic),
+                Padding = new Thickness(20)
+            };
+
+            var options = new ToastOptions
+            {
+                MessageOptions = messageOptions,
+                CornerRadius = new Thickness(40, 40, 40, 40),
+                
+                BackgroundColor = Color.FromHex("#CC0000")
+            };
+            Application.Current.MainPage.DisplayToastAsync(options);            
+        }
+    }
+}
